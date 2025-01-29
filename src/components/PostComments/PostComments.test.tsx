156cf22d47b3b7b966ef52ea0786759e0519f395
edilson591 <edilson591@gmail.com>
2025-01-28 22:11:18 -0300

    inserção do teste de renderização de dois componentes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,36 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+// import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  test("Deve renderizar o componente corretamente", () => {
+    render(<Post />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve renderizar dois comentarios", () => {
+    render(<Post />);
+
+    const input = screen.getByTestId("comentar");
+    const button = screen.getByTestId("botao-enviar");
+
+    fireEvent.change(input, {
+      target: {
+        value: "primeiro comentario",
+      },
     });
+    fireEvent.click(button);
+
+    expect(screen.getByText("primeiro comentario")).toBeInTheDocument();
+
+    fireEvent.change(input, {
+      target: {
+        value: "segundo comentario",
+      },
+    });
+    fireEvent.click(button);
+
+    expect(screen.getByText("segundo comentario")).toBeInTheDocument();
+  });
+
 });
--- src/components/PostComments/index.tsx
+++ src/components/PostComments/index.tsx
@@ -26,8 +26,8 @@ const Post = () => {
                 ))}
             </ul>
             <form onSubmit={handleAddComment} className={styles['post-comments-form']}>
-                <textarea value={tempComment} onChange={e => setTempComment(e.target.value)} required className={styles['post-comments-form-textarea']} />
-                <button type="submit" className={styles['post-comments-form-button']}>
+                <textarea value={tempComment} onChange={e => setTempComment(e.target.value)} required className={styles['post-comments-form-textarea']}  data-testid="comentar"/>
+                <button type="submit" className={styles['post-comments-form-button']} data-testid="botao-enviar">
                     Comentar
                 </button>
             </form>
